efe02278dab895bdfbd24d5110eaed3ca961567a
IOTech <68947790+IOTech17@users.noreply.github.com>
2025-03-27 13:25:11 +0100

    updated fcos version

--- vmsetup.sh
+++ vmsetup.sh
@@ -20,7 +20,7 @@ TEMPLATE_IGNITION="fcos-base-tmplt.yaml"
 
 # fcos version
 STREAMS=stable
-VERSION=40.20240906.3.0
+VERSION=41.20250302.3.2
 PLATEFORM=qemu
 BASEURL=https://builds.coreos.fedoraproject.org
 
